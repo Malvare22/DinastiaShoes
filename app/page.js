d04157dc71b5799432dc130216015678517a570d
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2025-04-07 20:04:43 -0500

    A

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
-import Image from 'next/image'
-import Navbar from './components/navbar'
-import { Footer } from './components/footer'
-import { HomeCardsGroup } from './components/products/homeCard'
-import { ImageCarousel, IndexImageCarousel } from './components/imageCarousel';
-import { getProductsDestacados } from './lib/products';
+import Image from "next/image";
+import Navbar from "./components/navbar";
+import { Footer } from "./components/footer";
+import { HomeCardsGroup } from "./components/products/homeCard";
+import { ImageCarousel, IndexImageCarousel } from "./components/imageCarousel";
+import { getProductsDestacados } from "./lib/products";
 
 export default function Home() {
-
-  const images = [ "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg", "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg", "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg"];
+  const images = [
+    "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+  ];
 
   // const [data, setData] = useState({});
-  console.log(url_backend)
-
+  console.log(url_backend);
 
   // useEffect(
   //   () => {
@@ -20,17 +22,12 @@ export default function Home() {
   // )
 
   return (
-      <>
-        
-          {/* <IndexImageCarousel></IndexImageCarousel> */}
-          <h1 className='text-black text-center font-sans text-3xl my-7 font-semibold'>PRODUCTOS DESTACADOS</h1>
-          {/* <HomeCardsGroup></HomeCardsGroup> */}
-        
-      </>
-  )
+    <>
+      {/* <IndexImageCarousel></IndexImageCarousel> */}
+      <h1 className="text-black text-center font-sans text-3xl my-7 font-semibold">
+        PRODUCTOS DESTACADOS
+      </h1>
+      {/* <HomeCardsGroup></HomeCardsGroup> */}
+    </>
+  );
 }
-
-
- 
-
-
