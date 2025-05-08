4b439da414c720d042cc9567c335b0ae01a907dd
JulianQuirozG <75459275+JulianQuirozG@users.noreply.github.com>
2025-05-08 16:45:08 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-}
-
-module.exports = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 }
+
+module.exports = {
+    nextConfig,
+    images: {
+    domains: ['fakestoreapi.com'], // Agrega aquí el dominio de tu servidor de origen de imágenes
+  },
+}
+
